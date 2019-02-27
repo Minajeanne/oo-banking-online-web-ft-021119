0ea00f0edfdd8152510f9c1b03c1a65e68bf527c
Alicia Santiago <albrown07@mac.com>
2019-02-27 19:36:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,6 +20,8 @@ attr_accessor :sender, :receiver, :amount, :status
     if @sender.balance < @amount
         @status = "rejected"
         "Transaction rejected. Please check your account balance."
+    elsif
+      @status == "complete"
     else
       @sender.deposit(@amount * -1) #how to account for amount?
       @receiver.deposit(@amount)
